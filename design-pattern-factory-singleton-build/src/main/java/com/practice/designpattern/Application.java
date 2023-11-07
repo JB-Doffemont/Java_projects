61e3f4933f5bd24d98fd34651fe33d53ffecbc04
JB <jeanbernard_doffemont@yahoo.fr>
2023-11-07 19:01:34 +0100

    Creation liste + impression liste

--- design-pattern-factory-singleton-build/src/main/java/com/practice/designpattern/Application.java
+++ design-pattern-factory-singleton-build/src/main/java/com/practice/designpattern/Application.java
@@ -3,6 +3,11 @@ package com.practice.designpattern;
 import com.practice.designpattern.pizza.PizzaFactory;
 import com.practice.designpattern.pizza.PizzaInterface;
 
+import java.util.List;
+
+import com.practice.designpattern.commande.Commande;
+import com.practice.designpattern.commande.CommandeManager;	
+
 public class Application {
 
 	public static void main(String[] args) {
@@ -11,6 +16,17 @@ public class Application {
 
 		PizzaInterface pizza1 = PizzaFactory.createPizza("Vegetarienne");
 		pizza1.cuire();
+		
+		Commande nouvelleCommande = new Commande(182, "Fromage", 1, "3 rue de la pizza", "En cours de livraison");
+		Commande nouvelleCommande1 = new Commande(183, "Vegetarienne", 1, "2 boulevard du médecin", "En cours de livraison");
+		
+		CommandeManager commandeManager = CommandeManager.getinstance();
+		
+		List<Commande> commandes = commandeManager.getlisteCommandes();
+		commandeManager.ajouterCommande(nouvelleCommande);
+		commandeManager.ajouterCommande(nouvelleCommande1);
+		
+		commandeManager.imprimerListeCommandes(commandes);
 	}
 
 }
--- design-pattern-factory-singleton-build/src/main/java/com/practice/designpattern/commande/CommandeManager.java
+++ design-pattern-factory-singleton-build/src/main/java/com/practice/designpattern/commande/CommandeManager.java
@@ -1,17 +1,40 @@
 package com.practice.designpattern.commande;
 
+import java.util.*;
+
 public class CommandeManager {
 
-	private static  CommandeManager instance;
-	
+	private List<Commande> commandes = new ArrayList<>();
+
+	private static CommandeManager instance;
+
 	private CommandeManager() {
-		
+
 	}
-	
-	public static  CommandeManager getinstance() {
+
+	public static CommandeManager getinstance() {
 		if (instance == null) {
 			instance = new CommandeManager();
 		}
 		return instance;
+	}
+	
+	public List<Commande> getlisteCommandes() {
+		return commandes;
+	}
+
+	public void ajouterCommande(Commande nouvelleCommande) {
+		commandes.add(nouvelleCommande);
+	}
+
+	public void imprimerListeCommandes(List<Commande> commandes) {
+	        for (Commande commande : commandes) {
+	            System.out.println("Numéro de commande : " + commande.getNumero());
+	            System.out.println("Type de pizza : " + commande.getTypePizza());
+	            System.out.println("Quantité : " + commande.getQuantite());
+	            System.out.println("Adresse de livraison : " + commande.getAdresseLivraison());
+	            System.out.println("État : " + commande.getEtat());
+	            System.out.println(); // Ajoute une ligne vide entre chaque commande
+	        }
 	}
 }
